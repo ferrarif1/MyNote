12a15a91e2d3be8d09b002fb17bc12adcfd3795e
ferrarif1 <15858188446@163.com>
2025-03-08 19:45:49 +0800

    update

--- Wallet/Old-coinstart_wallet_extension/web/test.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.sendMessage({ type: "show" }, (response) => {
-    if (response && response.hasOwnProperty("runtime")) {
-        if (response.runtime) {
-            console.log("send show success!");
-            console.log(response);
-        }
-    }
-});
-
